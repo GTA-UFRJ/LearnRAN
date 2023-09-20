09911fc89ced3b9d56035e1c6ad5f07379a199f2
mssvivian <144699344+mssvivian@users.noreply.github.com>
2023-09-20 10:38:48 -0300

    Update Naive_bayes.py

--- Naive_bayes.py
+++ Naive_bayes.py
@@ -24,12 +24,6 @@ for j in l2:
     mtc[j] = ues[j]
 for m in l3: 
     urllc[m] = ues[m]
-'''a = list(rng.choice(l1, size = 4, replace=False))
-b = list(rng.choice(l2, size = 4, replace=False))
-c = list(rng.choice(l3, size = 5, replace=False))
-a1 = list(rng.choice(l1, size = 4, replace=False))
-b1 = list(rng.choice(l2, size = 4, replace=False))
-c1 = list(rng.choice(l3, size = 5, replace=False))'''
 a = list(rng.choice(l1, size = 8, replace=False))
 b = list(rng.choice(l2, size = 8, replace=False))
 c = list(rng.choice(l3, size = 10, replace=False))
