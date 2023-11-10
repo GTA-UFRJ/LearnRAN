fe50e46b5dc2751ab925f657e5efc272017816fc
mssvivian <vivian@freguesia.gta.ufrj.br>
2023-11-10 07:58:00 -0300

    melhorando a disposição das funções do classficador

--- KFold Classifier/KFold_v1.py
+++ KFold Classifier/KFold_v1.py
@@ -1,8 +1,8 @@
 from sklearn.naive_bayes import GaussianNB
+from sklearn import utils
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import (
     accuracy_score,
     f1_score)
@@ -10,9 +10,9 @@ import numpy as np
 from itertools import combinations
 import os
 import configparser
-import csv
+import pandas as pd
+# import csv
 from sklearn.preprocessing import StandardScaler
-inf = 10**308
 
 configuration_file = '/lab/users/Cruz/vivian/LearnRAN/KFold Classifier/KFold_v1.ini'
  
@@ -37,9 +37,51 @@ embb_ues = config['DEFAULT']['default_embb_ues']
 mtc_ues = config['DEFAULT']['default_mtc_ues']
 urllc_ues = config['DEFAULT']['default_urllc_ues']
 
-classifier = KNeighborsClassifier(n_neighbors=3)
+possible_cases = [rome_slow_close_dir, rome_static_close_dir, rome_static_far_dir, rome_static_medium_dir]
+
+pd.options.display.max_rows = 9999
+
+classifier = GaussianNB()
 ues = {}
 embb_ues_data, mtc_ues_data, urllc_ues_data = [],[],[]
+n_embb_ues, n_mtc_ues, n_urllc_ues = 0,0,0
+def data_processing (data_list,n_ue):
+    dl_mcs, dl_turbo, dl_brate, ul_mcs, ul_brate = 0,0,0,0,0
+    last_time = 0
+    for j in range (1,len(data_list)):
+        time = data_list[j][0] - data_list[j-1][0]
+        if np.isinf(data_list[j-1][1]) or np.isinf(data_list[j-1][2]) or np.isinf(data_list[j-1][3]) or np.isinf(data_list[j-1][4]) or np.isinf(data_list[j-1][5]): continue 
+        last_time += time
+        dl_mcs += data_list[j-1][1] * time
+        dl_turbo += data_list[j-1][2] * time
+        dl_brate += data_list[j-1][3] * time
+        ul_mcs += data_list[j-1][4] * time
+        ul_brate += data_list[j-1][5] * time
+
+    dl_mcs = dl_mcs/last_time
+    dl_turbo = dl_turbo/last_time
+    dl_brate = dl_brate/last_time
+    ul_mcs = ul_mcs/last_time
+    ul_brate = ul_brate/last_time                  
+    data = [dl_mcs, dl_turbo, dl_brate, ul_mcs, ul_brate]
+
+    if str(n_ue) in embb_ues: 
+        class_ue = 'embb'
+        global embb_ues_data 
+        embb_ues_data.append(data)
+    elif str(n_ue) in mtc_ues:
+        class_ue = 'mtc'
+        global mtc_ues_data 
+        mtc_ues_data.append(data)
+    else: 
+        class_ue = 'urllc'
+        global urllc_ues_data 
+        urllc_ues_data.append(data)
+    return data,class_ue
+
+# opening files and calling data_processing
+ue_data, ue_classes = [],[]
+wished_cols = [0,7,9,10,13,15]
 for n_tr in range (18):
     tr = 'tr' + str(n_tr) + '/'
     for n_exp in range (1,7): 
@@ -51,183 +93,19 @@ for n_tr in range (18):
                 n_bs = n_ue//10 
             bs = 'bs' + str (n_bs) + '/'
             a = 'ue'+str(n_ue)+'.csv'
-            try:
-                with open (rome_slow_close_dir+tr+exp+bs+a) as b:
-                    file_reader_rswc = csv.reader(b, delimiter=',') 
-                    f = []
-                    data = []
-                    time = 0
-                    for i in file_reader_rswc:
-                        time += 1
-                        if time == 1: pass
-                        try:
-                            i = [i[0]] + [i[7]] + [i[9]] + [i[10]] + [i[13]] + [i[15]]                               
-                            i = [float(k) for k in i]             
-                            f.append(i)
-                        except ValueError: pass
-                        except IndexError: pass
-                    while len(f[0]) != len(f[-1]):
-                        f.remove(f[-1])
-                dl_mcs, dl_turbo, dl_brate, ul_mcs, ul_brate = 0,0,0,0,0
-                last_time = 0
-                for j in range (1,len(f)):
-                    time = f[j][0] - f[j-1][0]
-                    if np.isinf(f[j-1][1]) or np.isinf(f[j-1][2]) or np.isinf(f[j-1][3]) or np.isinf(f[j-1][4]) or np.isinf(f[j-1][5]): continue 
-                    last_time += time
-                    dl_mcs += f[j-1][1] * time
-                    dl_turbo += f[j-1][2] * time
-                    dl_brate += f[j-1][3] * time
-                    ul_mcs += f[j-1][4] * time
-                    ul_brate += f[j-1][5] * time
-                dl_mcs = round(dl_mcs/last_time,5)
-                dl_turbo = round(dl_turbo/last_time,5)
-                dl_brate = round(dl_brate/last_time,5)
-                ul_mcs = round(ul_mcs/last_time,5)
-                ul_brate = round(ul_brate/last_time,5)                   
-                data.append(dl_mcs);data.append(dl_turbo)
-                data.append(dl_brate);data.append(ul_mcs);data.append(ul_brate)
-                ues[rome_slow_close_dir+tr+exp+bs+a] = data
+            for traffic_case in possible_cases:
+                try:
+                    inf_ue = pd.read_csv(traffic_case+tr+exp+bs+a, skiprows=1, usecols = wished_cols,dtype=np.float64)
+                    inf_ue = np.array(inf_ue)
+                    data = data_processing (inf_ue,n_ue)
+                    ues[traffic_case+tr+exp+bs+a] = data
+                    #ue_data.append(data[0])
+                    #ue_classes.append(data[1])
+                except FileNotFoundError: pass
 
-                if str(n_ue) in embb_ues: 
-                    embb_ues_data.append(data)
-                elif str(n_ue) in mtc_ues:
-                    mtc_ues_data.append(data)
-                else: 
-                    urllc_ues_data.append(data)
-            except FileNotFoundError: pass
-
-            try:
-                with open (rome_static_close_dir+tr+exp+bs+a) as c:
-                    file_reader_rscc= csv.reader(c, delimiter=',') 
-                    f = []
-                    data = []
-                    time = 0
-                    for i in file_reader_rscc:
-                        time += 1
-                        if time == 1: pass
-                        try:
-                            i = [i[0]] + [i[7]] + [i[9]] + [i[10]] + [i[13]] + [i[15]]                               
-                            i = [float(k) for k in i]             
-                            f.append(i)
-                        except ValueError: pass
-                        except IndexError: pass
-                    while len(f[0]) != len(f[-1]):
-                        f.remove(f[-1])
-                dl_mcs, dl_turbo, dl_brate, ul_mcs, ul_brate = 0,0,0,0,0
-                last_time = 0
-                for j in range (1,len(f)):
-                    time = f[j][0] - f[j-1][0]
-                    if np.isinf(f[j-1][1]) or np.isinf(f[j-1][2]) or np.isinf(f[j-1][3]) or np.isinf(f[j-1][4]) or np.isinf(f[j-1][5]): continue 
-                    last_time += time
-                    dl_mcs += f[j-1][1] * time
-                    dl_turbo += f[j-1][2] * time
-                    dl_brate += f[j-1][3] * time
-                    ul_mcs += f[j-1][4] * time
-                    ul_brate += f[j-1][5] * time
-                dl_mcs = round(dl_mcs/last_time,5)
-                dl_turbo = round(dl_turbo/last_time,5)
-                dl_brate = round(dl_brate/last_time,5)
-                ul_mcs = round(ul_mcs/last_time,5)
-                ul_brate = round(ul_brate/last_time,5)
-                data.append(dl_mcs);data.append(dl_turbo)
-                data.append(dl_brate);data.append(ul_mcs);data.append(ul_brate)
-                ues[rome_static_close_dir+tr+exp+bs+a] = data
-                if str(n_ue) in embb_ues: 
-                    embb_ues_data.append(data)
-                elif str(n_ue) in mtc_ues:
-                    mtc_ues_data.append(data)
-                else: 
-                    urllc_ues_data.append(data)
-            except FileNotFoundError: pass
-
-            try:
-                with open (rome_static_far_dir+tr+exp+bs+a) as d:
-                    file_reader_rscf = csv.reader(d, delimiter=',') 
-                    f = []
-                    data = []
-                    time = 0
-                    for i in file_reader_rscf:
-                        time += 1
-                        if time == 1: pass
-                        try:
-                            i = [i[0]] + [i[7]] + [i[9]] + [i[10]] + [i[13]] + [i[15]]                               
-                            i = [float(k) for k in i]             
-                            f.append(i)
-                        except ValueError: pass
-                        except IndexError: pass
-                    while len(f[0]) != len(f[-1]):
-                        f.remove(f[-1])
-                dl_mcs, dl_turbo, dl_brate, ul_mcs, ul_brate = 0,0,0,0,0
-                last_time = 0
-                for j in range (1,len(f)):
-                    time = f[j][0] - f[j-1][0]
-                    if np.isinf(f[j-1][1]) or np.isinf(f[j-1][2]) or np.isinf(f[j-1][3]) or np.isinf(f[j-1][4]) or np.isinf(f[j-1][5]): continue 
-                    last_time += time
-                    dl_mcs += f[j-1][1] * time
-                    dl_turbo += f[j-1][2] * time
-                    dl_brate += f[j-1][3] * time
-                    ul_mcs += f[j-1][4] * time
-                    ul_brate += f[j-1][5] * time
-                dl_mcs = round(dl_mcs/last_time,5)
-                dl_turbo = round(dl_turbo/last_time,5)
-                dl_brate = round(dl_brate/last_time,5)
-                ul_mcs = round(ul_mcs/last_time,5)
-                ul_brate = round(ul_brate/last_time,5)         
-                data.append(dl_mcs);data.append(dl_turbo)
-                data.append(dl_brate);data.append(ul_mcs);data.append(ul_brate)
-                ues[rome_static_far_dir+tr+exp+bs+a] = data
-                if str(n_ue) in embb_ues: 
-                    embb_ues_data.append(data)
-                elif str(n_ue) in mtc_ues:
-                    mtc_ues_data.append(data)
-                else: 
-                    urllc_ues_data.append(data)
-            except FileNotFoundError: pass
-            try:
-                with open (rome_static_medium_dir+tr+exp+bs+a) as e:
-                    file_reader_rscm = csv.reader(e, delimiter=',') 
-                    f = []
-                    data = []
-                    time = 0
-                    for i in file_reader_rscm:
-                        time += 1
-                        if time == 1: pass
-                        try:
-                            i = [i[0]] + [i[7]] + [i[9]] + [i[10]] + [i[13]] + [i[15]]                               
-                            i = [float(k) for k in i]             
-                            f.append(i)
-                        except ValueError: pass
-                        except IndexError: pass
-                    while len(f[0]) != len(f[-1]):
-                        f.remove(f[-1])
-                dl_mcs, dl_turbo, dl_brate, ul_mcs, ul_brate = 0,0,0,0,0
-                last_time = 0
-                for j in range (1,len(f)):
-                    time = f[j][0] - f[j-1][0]
-                    if np.isinf(f[j-1][1]) or np.isinf(f[j-1][2]) or np.isinf(f[j-1][3]) or np.isinf(f[j-1][4]) or np.isinf(f[j-1][5]): continue 
-                    last_time += time
-                    dl_mcs += f[j-1][1] * time
-                    dl_turbo += f[j-1][2] * time
-                    dl_brate += f[j-1][3] * time
-                    ul_mcs += f[j-1][4] * time
-                    ul_brate += f[j-1][5] * time
-                dl_mcs = round(dl_mcs/last_time,5)
-                dl_turbo = round(dl_turbo/last_time,5)
-                dl_brate = round(dl_brate/last_time,5)
-                ul_mcs = round(ul_mcs/last_time,5)
-                ul_brate = round(ul_brate/last_time,5)                   
-                data.append(dl_mcs);data.append(dl_turbo)
-                data.append(dl_brate);data.append(ul_mcs);data.append(ul_brate)
-                ues[rome_static_medium_dir+tr+exp+bs+a] = data
-                if str(n_ue) in embb_ues: 
-                    embb_ues_data.append(data)
-                elif str(n_ue) in mtc_ues:
-                    mtc_ues_data.append(data)
-                else: 
-                    urllc_ues_data.append(data)
-            except FileNotFoundError: pass
-def K_Fold (a,k_fold,b,embb_ues_data,mtc_ues_data,urllc_ues_data):
+def data_used (b,c):
     rng = np.random.default_rng(seed=b)
+    print (embb_ues_data)
     len_embb = len(embb_ues_data)//3
     len_mtc = len(mtc_ues_data)//3
     len_urllc = len(urllc_ues_data)//3
@@ -243,25 +121,24 @@ def K_Fold (a,k_fold,b,embb_ues_data,mtc_ues_data,urllc_ues_data):
     mtc_ues_data_notused = mtc_ues_data[:len_mtc]
     urllc_ues_data_used = urllc_ues_data[len_urllc:]
     urllc_ues_data_notused = urllc_ues_data[:len_urllc]
-    
-    ue_class,ue_data = [],[]
-    for i in embb_ues_data_used:
-        ue_data.append(i)
-        ue_class.append('embb')
-    for j in mtc_ues_data_used:
-        ue_data.append(j)
-        ue_class.append('mtc')
-    for k in urllc_ues_data_used:
-        ue_data.append(k)
-        ue_class.append('urllc')
-    ue_data = np.array(ue_data)
-    ue_class = np.array(ue_class)
-    ue_data2 = ue_data
+    ue_data = embb_ues_data_used + mtc_ues_data_used + mtc_ues_data_used
+    ue_classes = ['embb'] * len(embb_ues_data_used) + ['mtc'] * len(mtc_ues_data_used) + ['urllc'] * len(urllc_ues_data_used)
+    #x,y =  utils.shuffle(ue_data, ue_classes,random_state = c)
+    return ue_data,ue_classes
+
+
+def K_Fold (a,k_fold,b,c):
+    ue_data_classifier, ue_classes_classifier = data_used (b,c)
+    ue_data_classifier = np.array(ue_data)
+    ue_classes_classifier = np.array(ue_classes)
+    print (ue_classes_classifier)
+    print (ue_data_classifier)
+
     kf = StratifiedKFold(n_splits=k_fold, shuffle=True, random_state=a)
     accuracy_scores = []
-    for train_index, test_index in kf.split(ue_data,ue_class):
-        data_train, data_test = ue_data[train_index], ue_data[test_index]
-        class_train, class_test = ue_class[train_index], ue_class[test_index]
+    for train_index, test_index in kf.split(ue_data_classifier,ue_classes_classifier):
+        data_train, data_test = ue_data_classifier[train_index], ue_data_classifier[test_index]
+        class_train, class_test = ue_classes_classifier[train_index], ue_classes_classifier[test_index]
         classifier.fit(data_train, class_train)
         predictions = classifier.predict(data_test)
         accuracy = accuracy_score(predictions,class_test)
@@ -275,7 +152,7 @@ def K_Fold (a,k_fold,b,embb_ues_data,mtc_ues_data,urllc_ues_data):
     result = str(mean_accuracy) + ' ; ' + str(std_accuracy)
     return result
     
-print(K_Fold (8,3,1,embb_ues_data,mtc_ues_data,urllc_ues_data))
+print(K_Fold (8,3,1,5))
 
 
 
